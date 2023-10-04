2735fde3b209a8c39a361e055ef81de35722f69b
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-10-04 19:29:40 +0900

    sdd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,6 @@
 import { Inter } from "next/font/google";
+// @ts-ignore
+import { Helmet } from "react-helmet";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -7,6 +9,10 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+      <Helmet>
+        <title>s0u7a.net</title>
+        <link rel="icon" href="/next.svg" />
+      </Helmet>
       <div className="z-10 max-w-5xl w-full flex flex-col items-center justify-between font-mono text-sm lg:flex">
         <h1>Welcome to s0u7a.net!</h1>
         <h2>s0u7a@s0u7a.net &gt; ~/s0u7a.net $ bun dev</h2>
